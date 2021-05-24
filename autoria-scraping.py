6efeea9635ef0e10c3fd5596b3ed7e30235effd0
SillySalat <sol.varga@gmail.com>
2021-05-24 20:03:33 +0300

    basic stracture

--- autoria-scraping.py
+++ autoria-scraping.py
@@ -1,2 +1,8 @@
 import requests
 import bs4
+
+page = requests.request('GET', 'https://auto.ria.com/uk/auto_mazda_6_29640989.html')
+page.raise_for_status()
+soup = bs4.BeautifulSoup(page.content, "html.parser")
+photo = soup.select('picture')
+print(photo[0].getText())
